7f38637731382d4ba565112f0b63109550694f55
selvan <p.thamarai@gmail.com>
2021-09-15 11:47:20 +0530

    Latest

--- android/app/src/main/java/com/example/rnandandroid/CustomFrameLayout.java
+++ android/app/src/main/java/com/example/rnandandroid/CustomFrameLayout.java
@@ -7,6 +7,12 @@ import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+/*
+    https://github.com/facebook/react-native/issues/11829
+    https://stackoverflow.com/questions/46609588/native-android-ui-component-not-updating
+    Solution - https://github.com/tlcheah2/test/commit/9a70c2b985769a2a8277219d104b0893b406eb49
+    Solution - https://github.com/opentok/opentok-react-native/blob/main/android/src/main/java/com/opentokreactnative/OTSubscriberLayout.java
+ */
 public class CustomFrameLayout extends FrameLayout {
 
     public CustomFrameLayout(@NonNull Context context) {
